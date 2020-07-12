eca425c18481a7ca6f05fb4469f5b2b6b3d1ffe2
Dave Bakshani <dave@bakshani.net>
2020-07-11 21:06:37 -0700

    Relocate volume and media playback controls

--- keyboards/xd75/keymaps/dbakshani/keymap.c
+++ keyboards/xd75/keymaps/dbakshani/keymap.c
@@ -49,18 +49,11 @@ enum custom_keycodes {
 };
 
 //TODO: add numpad
-//TODO: v- switch alt + gui for mac
-//TODO: relocate volume controls
-//TODO: relocate media controls
-//TODO: v- remove temp reset from main layer
-//TODO: v- make reset hard to accidentally activate
 //TODO: make rgb toggle easy to use
 //TODO: implement all rgb controls
 //TODO: add printscreen, scroll lock, pause
-//TODO: v- add function keys back to top row
 //TODO: add define/aliases for long keys
 //TODO: add colemak and dvorak
-//TODO: v- remove sound controls
 //TODO: cleanup spacing
 //TODO: 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -115,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+--------+--------+--------+------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |        |        |        |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+------+--------+--------+---------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |        |        |        |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |      | VOL- | MUTE | VOL+ |      |        |        |        |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+--------+--------+--------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |        |        |        |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      | STOP | PREV | PLAY | NEXT |      |        |        |        |      |ISO ~ |ISO | | Home | End  |      |
  * |------+------+------+------+------+---------------+--------+--------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |        |        |        |      |      | Next | Vol- | Vol+ | Play |
  * `--------------------------------------------------------------------------------------------------------------'
@@ -125,8 +118,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_ortho_5x15(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______, KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
+    KC_DEL,  KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, _______, _______,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, KC_MSTP, KC_MPRV,  KC_MPLY,  KC_MNXT, _______, _______, _______, _______, _______,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
   ),
 
@@ -136,9 +129,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+--------+--------+--------+------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |        |        |        |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+--------+--------+---------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |        |        |        |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |      | VOL- | MUTE | VOL+ |      |        |        |        |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------+--------+--------+--------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |        |        |        |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      | STOP | PREV | PLAY | NEXT |      |        |        |        |      |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+---------------+--------+--------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |        |        |        |      |      | Prev | Vol- | Vol+ | Play |
  * `--------------------------------------------------------------------------------------------------------------'
@@ -146,8 +139,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_ortho_5x15(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   _______, _______, _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  _______, _______, _______,  KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, _______, _______, _______,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    KC_DEL,  KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, _______,  _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, KC_MSTP, KC_MPRV,  KC_MPLY,  KC_MNXT,  _______, _______, _______, _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY
   ),
 
