22c1af0f493cfdb2e972bb9f17aee0bc86bd4416
Dave Bakshani <dave@bakshani.net>
2020-07-12 20:39:05 -0700

    Fix numpad to lower layer w/o num lock, for XD75

--- keyboards/xd75/keymaps/dbakshani/keymap.c
+++ keyboards/xd75/keymaps/dbakshani/keymap.c
@@ -124,23 +124,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  NUM LK | P/     | P*     | F7     |  F8  |  F9  | F10  | F11  | F12  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |         |  /     |  *     | F7     |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------+---------+--------+--------+--------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |  P7     | P8     | P9     |   6    |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   7     |  8     |  9     |   6    |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+---------+--------+--------+---------------+------+------+------+------|
- * | Del  |      | VOL- | MUTE | VOL+ |      |  P4     | P5     | P6     | +      |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |      | VOL- | MUTE | VOL+ |      |   4     |  5     |  6     | +      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------+---------+--------+--------+--------+------+------+------+------+------|
- * |      | STOP | PREV | PLAY | NEXT |      |  P1     | P2     | P3     | -      |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      | STOP | PREV | PLAY | NEXT |      |   1     |  2     |  3     | -      |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+----------------+--------+--------+--------+------+------+------+------+------|
- * |      |      |      |      |      |      |  P0     |        | P.     | PENT   |      | Prev | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |   0     |        |  .     | ENTER  |      | Prev | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------------------------------------'
  */
   [_LOWER] = LAYOUT_ortho_5x15(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_P7,   KC_P8,   KC_P9,   KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_DEL,  _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, KC_P4,   KC_P5,   KC_P6,   KC_PLUS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_MSTP, KC_MPRV,  KC_MPLY,  KC_MNXT,  _______, KC_P1,   KC_P2,   KC_P3,   KC_MINS,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, KC_P0,   _______, KC_PDOT, KC_PENT,  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_7,   KC_8,   KC_9,   KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_DEL,  _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, KC_4,   KC_5,   KC_6,   KC_PLUS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, KC_MSTP, KC_MPRV,  KC_MPLY,  KC_MNXT,  _______, KC_1,   KC_2,   KC_3, KC_MINS,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, KC_0,   _______, KC_DOT, KC_ENT,  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY
   ),
 
 /* Adjust (Lower + Raise)
