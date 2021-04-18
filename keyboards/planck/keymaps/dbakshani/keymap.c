47d5c02b336f75fe8f6dc9d6ffd5c9e1dae6a936
Dave Bakshani <dave@bakshani.net>
2021-04-18 16:08:46 -0700

    Implement home row modifiers

--- keyboards/planck/keymaps/dbakshani/keymap.c
+++ keyboards/planck/keymaps/dbakshani/keymap.c
@@ -47,17 +47,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |A/ALT |S/GUI |D/SHFT|F/CTRL|   G  |   H  |J/CTRL|K/SHFT|L/GUI |;/ALT |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |X/ALGR|   C  |   V  |   B  |   N  |   M  |   ,  |./ALGR|   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    HYPR_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    LSFT_T(KC_ENT), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT) ,
+    HYPR_T(KC_ESC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN), KC_QUOT,
+    LSFT_T(KC_ENT), KC_Z, ALGR_T(KC_X),    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, ALGR_T(KC_DOT),  KC_SLSH, RSFT_T(KC_ENT) ,
     BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(_NAVIGATION, KC_SPC), LT(_NAVIGATION, KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
