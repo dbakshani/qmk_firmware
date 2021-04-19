880923be92886b3657bb032e84859e2dda1dd99f
Dave Bakshani <dave@bakshani.net>
2021-04-18 17:43:54 -0700

    Add defines and adjust QwERTY layer formatting

--- keyboards/planck/keymaps/dbakshani/keymap.c
+++ keyboards/planck/keymaps/dbakshani/keymap.c
@@ -41,24 +41,49 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
+#define LSH_ENT LSFT_T(KC_ENT)
+#define RSH_ENT RSFT_T(KC_ENT)
+#define HYP_ESC HYPR_T(KC_ESC)
+#define NAV_SPC LT(_NAVIGATION, KC_SPC)
+#define DVK_DOT KC_E
+#define DVK_SLSH KC_LBRC
+#define DVK_PLS KC_RCBR
+#define DVK_MIN KC_QUOT
+
+// Left-hand home row mods
+#define ALT_A LALT_T(KC_A)
+#define GUI_S LGUI_T(KC_S)
+#define SFT_D LSFT_T(KC_D)
+#define CTL_F LCTL_T(KC_F)
+#define ALGR_X ALGR_T(KC_X)
+
+// Right-hand home row mods
+#define CTL_J RCTL_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define GUI_L RGUI_T(KC_L)
+#define ALT_SCLN LALT_T(KC_SCLN)
+#define ALGR_DOT ALGR_T(KC_DOT)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |A/ALT |S/GUI |D/SHFT|F/CTRL|   G  |   H  |J/CTRL|K/SHFT|L/GUI |;/ALT |  "   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |X/ALGR|   C  |   V  |   B  |   N  |   M  |   ,  |./ALGR|   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
+ * .-----------------------------------------------------------------------------------------------------------.
+ * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | BACKSP |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | HYP_ESC| A/ALT  | S/GUI  | D/SHFT | F/CTRL | G      | H      | J/CTRL | K/SHFT | L/GUI  | ;/ALT  | '      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | LSH_ENT| Z      | X/ALGR | C      | V      | B      | N      | M      | ,      | ./ALGR | /      | RSH_ENT|
+ * |--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | Brite  | LCTRL  | LALT   | LGUI   | LOWER  | NAV_SPC| NAV_SPC| RAISE  | Left   | Down   | Up     | Right  |
+ * '-----------------------------------------------------------------------------------------------------------'
  */
+
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    HYPR_T(KC_ESC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN), KC_QUOT,
-    LSFT_T(KC_ENT), KC_Z, ALGR_T(KC_X),    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, ALGR_T(KC_DOT),  KC_SLSH, RSFT_T(KC_ENT) ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(_NAVIGATION, KC_SPC), LT(_NAVIGATION, KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    HYP_ESC , ALT_A  , GUI_S  , SFT_D  , CTL_F  , KC_G   , KC_H   , CTL_J  , SFT_K  , GUI_L  ,ALT_SCLN, KC_QUOT,
+    LSH_ENT , KC_Z   , ALGR_X , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM,ALGR_DOT, KC_SLSH, RSH_ENT,
+    BACKLIT , KC_LCTL, KC_LALT, KC_LGUI, LOWER  , NAV_SPC, NAV_SPC, RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
 ),
 
 /* Colemak
