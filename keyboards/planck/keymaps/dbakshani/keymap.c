c220d0ef23d05ecafcc3f56a14f2f68571e082e4
Dave Bakshani <dave@bakshani.net>
2021-05-02 18:30:27 -0700

    Flesh out number/function and symbol layers

--- keyboards/planck/keymaps/dbakshani/keymap.c
+++ keyboards/planck/keymaps/dbakshani/keymap.c
@@ -44,7 +44,7 @@ enum planck_keycodes {
 #define LSH_ENT LSFT_T(KC_ENT)
 #define RSH_ENT RSFT_T(KC_ENT)
 #define HYP_ESC HYPR_T(KC_ESC)
-#define HYP_MIN HYPR_T(DVK_MIN)
+#define HYP_MIN HYPR_T(DVK_MINS)
 #define LSH_SPC LSFT_T(KC_SPC)
 #define NAV_ENT LT(_NAVIGATION, KC_ENT)
 #define LOWR_BSP LT(_LOWER, KC_BSPC)
@@ -53,10 +53,19 @@ enum planck_keycodes {
 #define MOUS_ESC LT(_MOUSE, KC_ESC)
 
 // make it easier to map qwerty to dvorak
+#define DVK_QUOT KC_Q
+#define DVK_COMM KC_W
 #define DVK_DOT KC_E
 #define DVK_SLSH KC_LBRC
-#define DVK_PLS KC_RCBR
-#define DVK_MIN KC_QUOT
+#define DVK_EQUL KC_RBRC
+#define DVK_PLUS KC_RCBR
+#define DVK_MINS KC_QUOT
+#define DVK_UNDS KC_DQUO
+#define DVK_SCLN KC_Z
+#define DVK_LBRC KC_MINS
+#define DVK_RBRC KC_EQL
+#define DVK_LCBR KC_UNDS
+#define DVK_RCBR KC_PLUS
 
 // Left-hand home row mods
 #define SFT_A LSFT_T(KC_A)
@@ -94,40 +103,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL  , KC_LCTL, KC_LALT,MDIA_TAB, LSH_SPC,MOUS_ESC, NAV_ENT,LOWR_BSP,RAIS_DEL, KC_DOWN, KC_LEFT, KC_RGHT
 ),
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-[_RAISE] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
-    KC_DEL , KC_F1  ,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7  ,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+[_LOWER] = LAYOUT_planck_grid(
+    KC_GRV ,DVK_LBRC, KC_7   , KC_8   , KC_9   ,DVK_RBRC, KC_PRSC, KC_F7  , KC_F8  , KC_F9,   KC_F12 , KC_DEL ,
+    KC_DEL , _______, KC_4   , KC_5   , KC_6   ,DVK_EQUL, KC_SCRL, KC_F4  , KC_F5  , KC_F6  , KC_F11 , _______,
+    _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_BSLS, KC_PAUS, KC_F1  , KC_F2  , KC_F3  , KC_F10 , _______,
+    _______, _______, _______, _______, KC_0   ,DVK_MINS, _______, _______, _______, _______, _______, _______,
 ),
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Prev | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-[_LOWER] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY
+[_RAISE] = LAYOUT_planck_grid(
+    KC_TILD, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______, _______, _______, _______, _______, KC_DEL ,
+    _______, _______, KC_DLR , KC_PERC, KC_CIRC,DVK_PLUS, _______, _______, _______, _______, _______, _______,
+    _______, KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_PIPE, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_LPRN, KC_RPRN,DVK_UNDS, _______, _______, _______, _______, _______, _______,
 ),
 
 /* Adjust (Lower + Raise)
