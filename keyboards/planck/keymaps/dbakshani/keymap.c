05177584c20b909ae4d38aa6fdf2ff48c9fede6d
Dave Bakshani <dave@bakshani.net>
2021-05-08 19:58:25 -0700

    Clean up number and symbols layers

--- keyboards/planck/keymaps/dbakshani/keymap.c
+++ keyboards/planck/keymaps/dbakshani/keymap.c
@@ -94,16 +94,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV  , KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,DVK_LBRC, KC_7   , KC_8   , KC_9   ,DVK_RBRC, KC_DEL ,
+    KC_GRV  , KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,DVK_PLUS, KC_7   , KC_8   , KC_9   , KC_ASTR, KC_DEL ,
     KC_DEL  , KC_F11 , KC_F4  , KC_F5  , KC_F6  , KC_DEL ,DVK_SLSH, KC_4   , KC_5   , KC_6   ,DVK_EQUL, _______,
      _______, KC_F10 , KC_F1  , KC_F2  , KC_F3  , KC_GRV , KC_BSLS, KC_1   , KC_2   , KC_3   , KC_SLCK, _______,
      _______, _______, _______, _______, _______, KC_PAUS,DVK_MINS, KC_0   , DVK_DOT, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD ,DVK_LCBR, KC_AMPR, KC_ASTR, KC_LPRN,DVK_RCBR, _______,DVK_LCBR,DVK_RCBR,DVK_QSTN,DVK_PLUS, KC_DEL ,
-     _______,DVK_QSTN, KC_DLR , KC_PERC, KC_CIRC,DVK_PLUS, _______,DVK_LBRC,DVK_RBRC,DVK_SLSH,DVK_EQUL, _______,
-     _______, KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_TILD, KC_PIPE, KC_LPRN, KC_RPRN, _______, _______, _______,
+    KC_TILD , _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,DVK_LCBR,DVK_RCBR,DVK_QSTN,DVK_PLUS, KC_DEL ,
+     _______, _______, KC_DLR , KC_PERC, KC_CIRC, _______, _______,DVK_LBRC,DVK_RBRC,DVK_SLSH,DVK_EQUL, _______,
+     _______, _______, KC_EXLM, KC_AT  , KC_HASH, KC_TILD, KC_PIPE, KC_LPRN, KC_RPRN, _______, _______, _______,
      _______, _______, _______, _______, _______,DVK_UNDS, _______, _______, _______, _______, _______, _______
 ),
 
